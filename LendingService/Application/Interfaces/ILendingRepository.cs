a0d8dc7ce70fe34f9744eb3090c8b7936b1be6b4
Peter Imade <peterosakpolor661@gmail.com>
2025-07-18 21:08:40 +0100

    added lending repository

---
+++ LendingService/Application/Interfaces/ILendingRepository.cs
@@ -0,0 +1,9 @@
+﻿using LendingService.Domain.Entities;
+
+namespace LendingService.Application.Interfaces
+{
+    public interface ILendingRepository
+    {
+        Task CreateAsync(Lending lending, CancellationToken cancellationToken);
+    }
+}
---
+++ LendingService/Infrastructure/Repositories/LendingRepository.cs
@@ -0,0 +1,21 @@
+﻿using LendingService.Application.Interfaces;
+using LendingService.Domain.Entities;
+using LendingService.Infrastructure.Data;
+
+namespace LendingService.Infrastructure.Repositories
+{
+    public class LendingRepository: ILendingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LendingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(Lending lending, CancellationToken cancellationToken)
+        {
+            await _context.Lendings.AddAsync(lending, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
